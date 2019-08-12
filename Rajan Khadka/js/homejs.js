99cd4bd9fb47b59654aaf7f4961c670fc7617e22
khdrajan <53071516+khdrajan@users.noreply.github.com>
2019-08-12 22:19:14 +0545

    Add files via upload

---
+++ Rajan Khadka/js/homejs.js
@@ -0,0 +1,17 @@
+image1= new Image();
+image1.src = "images/welcome.jpg";
+image2 = new Image();
+image2.src = "images/logohome.jpg";
+
+function not()
+{
+  document.roll.src = image2.src;
+  //return true;
+}
+
+function guess()
+{
+  document.roll.src = image1.src;
+  //return true;
+
+}
